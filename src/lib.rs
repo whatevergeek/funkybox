c40dda5f1c762357f0d26d1339ed41a221b4d524
Paul Lorett Amazona <whatevergeek@gmail.com>
2020-08-04 00:29:19 +0800

    added adjust_r2

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,17 @@ fn sum(a: usize, b: usize) -> PyResult<usize> {
     Ok(a + b)
 }
 
+#[pyfunction]
+fn adjust_r2(r2: f64, n: f64, p: f64) -> PyResult<f64> {
+    Ok(1.0 - (1.0 - r2) * ((n - 1.0) / (n - p - 1.0)))
+}
+
 /// This module is a python module implemented in Rust.
 #[pymodule]
 fn funkybox(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
     m.add_wrapped(wrap_pyfunction!(sum))?;
+    m.add_wrapped(wrap_pyfunction!(adjust_r2))?;
 
     Ok(())
 }
